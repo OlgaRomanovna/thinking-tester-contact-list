7b50240aed176ab6aaf559d1278bb7f570d95cd4
Olga B <o.lgaromanovna@yandex.ru>
2024-01-24 20:40:01 +0300

    fixed files

--- tests/web/conftest.py
+++ tests/web/conftest.py
@@ -5,10 +5,6 @@ from dotenv import load_dotenv
 from selene import browser
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-
-from data import data
-from tests.api.conftest import post_request
-from utils import const
 from utils.attachments import add_html, add_screenshot, add_video, add_logs
 
 DEFAULT_BROWSER_VERSION = '120'
@@ -50,28 +46,7 @@ def open_browser(request):
 def pytest_addoption(parser):
     parser.addoption('--browser_name', action='store', default="chrome",
                      help="Choose browser: chrome or firefox")
-    parser.addoption('--browser_version', action='store', default="99.0")
-
-
-@pytest.fixture()
-def add_contact_fixture():
-    with allure.step('Создаём контакт'):
-        json = {
-            "firstName": data.first_name,
-            "lastName": data.last_name,
-            "birthdate": data.birthdate,
-            "email": data.email,
-            "phone": data.phone,
-            "street1": data.street1,
-            "street2": data.street2,
-            "city": data.city,
-            "stateProvince": data.state_province,
-            "postalCode": data.postal_code,
-            "country": data.country
-        }
-        response = post_request(f'/contacts', json=json)
-        assert response.status_code == const.CREATED
-        return response
+    parser.addoption('--browser_version', action='store', default="120.0")
 
 
 @pytest.fixture(scope='session', autouse=True)
@@ -79,15 +54,3 @@ def load_env():
     load_dotenv()
 
 
-@pytest.fixture()
-def web_user_for_auth():
-    return UserModel(email=os.getenv('EMAIL'), password=os.getenv('PASSWORD_USER'))
-
-
-@pytest.fixture(scope='session')
-def get_user_data_from_env():
-    with allure.step('Получаем данные по пользователю'):
-        resp = auth(email=os.getenv('EMAIL'),
-                    password=os.getenv('PASSWORD_USER'))
-        assert resp.status_code == const.OK
-        return UserModel(token=resp.json()['token'], _id=resp.json()['user']['_id'])
